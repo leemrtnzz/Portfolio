8efcccda5d1f633d602e411eda49d2c63288fbbc
leemrtnzz <ichsan.haekal23@gmail.com>
2024-02-07 16:27:30 +0700

    perbaruan git aja

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+// Humberger
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const navItems1 = document.getElementById('nav-items-1');
+    const navItems2 = document.getElementById('nav-items-2');
+    const navItems3 = document.getElementById('nav-items-3');
+    const navItems4 = document.getElementById('nav-items-4');
+    const navItems5 = document.getElementById('nav-items-5');
+
+    const fixnav = header.offsetTop;
+    if (window.pageYOffset > fixnav) {
+        header.classList.add('nav-fixed');
+        navItems1.classList.add('nav-convert');
+        navItems2.classList.add('nav-convert');
+        navItems3.classList.add('nav-convert');
+        navItems4.classList.add('nav-convert');
+        navItems5.classList.add('nav-convert');
+
+    } else {
+        header.classList.remove('nav-fixed');
+        navItems1.classList.remove('nav-convert');
+        navItems2.classList.remove('nav-convert');
+        navItems3.classList.remove('nav-convert');
+        navItems4.classList.remove('nav-convert');
+        navItems5.classList.remove('nav-convert');
+
+        navItems1.classList.add('nav-first');
+        navItems2.classList.add('nav-first');
+        navItems3.classList.add('nav-first');
+        navItems4.classList.add('nav-first');
+        navItems5.classList.add('nav-first');
+    }
+}
+
+const humberger = document.getElementById('humberger');
+const navMenu = document.querySelector('#nav-menu');
+humberger.addEventListener('click', function (e) {
+    humberger.classList.toggle('humberger-active');
+    navMenu.classList.toggle('hidden');
+});
